f3162074e2241606397bf1feda32f52e182a81ce
Adelson <desouzaadelson@hotmail.com>
2024-01-30 17:28:24 -0300

    category_db, product_db, category_service, product_service, category_handler

---
+++ goapi/internal/webserver/category_handler.go
@@ -0,0 +1,7 @@
+package webserver
+
+import "github.com/adlsz94/e-commerce_fullcycle_imersao17/goapi/internal/service"
+
+type WebCategoryHandler struct {
+	CategoryService *service.CategoryService
+}
